c5d46d1bb039d003972c5d1d9356c6ee83d9dcc6
Chase Turner <chase.turner@resolvesoft.com>
2025-03-27 22:40:09 -0400

    chase changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'The Enchanted Haven';
+export class AppComponent implements OnInit {
+  isHomeRoute: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Check initial route
+    this.isHomeRoute = this.router.url === '/' || this.router.url === '/home';
+
+    // Subscribe to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomeRoute = event.url === '/' || event.url === '/home';
+      }
+    });
+  }
 }
